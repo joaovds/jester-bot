259965f953ffa79aeab3f1fd022b4b15ab39560a
João Victor da Silva <jv782063@gmail.com>
2023-05-29 22:23:15 -0300

    add godotenv e carrega o .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-  "log"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func loadEnv() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Erro ao carregar .env")
+  }
+}
+
 func main() {
+  loadEnv()
+
+  token := os.Getenv("DISCORD_BOT_TOKEN")
+  if token == "" {
+    log.Fatal("Token não especificado")
+  }
+  
   log.Println("Jester!")
 }
 

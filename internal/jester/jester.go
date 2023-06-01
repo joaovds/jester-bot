b61254fba6fe00eb7940f90e19b8f6f18e56b599
João Victor da Silva <jv782063@gmail.com>
2023-06-01 08:33:55 -0300

    reestrutura o projeto para poder organizar os futuros comandos

--- internal/jester/jester.go
+++ internal/jester/jester.go
@@ -4,13 +4,20 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+type Command interface {
+  HandleCommand(session *discordgo.Session, messageCreate *discordgo.MessageCreate) error
+}
+
 type Jester struct {
   session *discordgo.Session
+  prefix  string
+  commands map[string]Command
 }
 
 func NewJester(token string) (*Jester, error) {
@@ -19,14 +26,20 @@ func NewJester(token string) (*Jester, error) {
     return nil, err
   }
 
+  prefix := "!j"
+
   return &Jester{
-    session,
+    session: session,
+    prefix: prefix,
+    commands: make(map[string]Command),
   }, nil
 }
 
-func (jester *Jester) JesterRun() error {
-  jester.session.AddHandler(jester.handleCommand)
+func (jester *Jester) RegisterCommand(name string, command Command) {
+  jester.commands[name] = command
+}
 
+func (jester *Jester) JesterRun() error {
   err := jester.session.Open()
   if err != nil {
     return err
@@ -34,12 +47,14 @@ func (jester *Jester) JesterRun() error {
 
   log.Println("Jester em execução!... (Ctrl+C para parar)")
 
+  jester.session.AddHandler(jester.handleCommand)
+
   closeApp := make(chan os.Signal, 1)
 
   signal.Notify(closeApp, os.Interrupt, syscall.SIGTERM)
   <-closeApp
 
-  // jester.session.Close()
+  jester.session.Close()
 
   log.Println("Jester parado!...")
 
@@ -47,41 +62,21 @@ func (jester *Jester) JesterRun() error {
 }
 
 func (jester *Jester) handleCommand(session *discordgo.Session, messageCreate *discordgo.MessageCreate) {
-  if messageCreate.Content == "!jcls" {
-    log.Print("Limpando...")
+  if !strings.HasPrefix(messageCreate.Content, jester.prefix) {
+    return
+  }
 
-    channel, err := session.Channel(messageCreate.ChannelID)
-    if err != nil {
-      log.Println("Erro ao obter configurações do canal:", err)
-      return
-    }
+  args := strings.Split(messageCreate.Content, " ")
+  command := strings.TrimPrefix(args[0], jester.prefix)
 
-    _, err = session.ChannelDelete(messageCreate.ChannelID)
-    if err != nil {
-      log.Println("Erro ao excluir canal:", err)
-      return
-    }
+  commandInstance, ok := jester.commands[command]
+  if !ok {
+    return
+  }
 
-    newChannel, err := session.GuildChannelCreate(messageCreate.GuildID, channel.Name, discordgo.ChannelTypeGuildText)
-    if err != nil {
-      log.Println("Erro ao criar novo canal:", err)
-      return
-    }
-
-    newChannelConfig := discordgo.ChannelEdit{
-      Name: channel.Name,
-      Topic: channel.Topic,
-      PermissionOverwrites: channel.PermissionOverwrites,
-      ParentID: channel.ParentID,
-    }
-
-    _, err = session.ChannelEdit(newChannel.ID, &newChannelConfig)
-    if err != nil {
-			log.Println("Erro ao atualizar canal:", err)
-			return
-		}
-
-		log.Println("Canal excluído e recriado com sucesso!")
+  err := commandInstance.HandleCommand(session, messageCreate)
+  if err != nil {
+    log.Println("Erro ao executar comando:", err)
   }
 }
 
